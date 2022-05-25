4f6ecf24239bc12b37a8d1a93acff9d7c4b8ef40
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-05-25 15:45:43 +0100

    prepare router

--- rest_mongo_db/db/connect.js
+++ rest_mongo_db/db/connect.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
+const mongoDebug = require('debug')('app:mongo');
 
 mongoose.connect('mongodb+srv://user:1234@cluster0.l9vwd.mongodb.net/?retryWrites=true&w=majority')
-.then(()=>console.log('MongoDB is UP.'))
-.catch(err=>console.log('MongoDB is Down, raison :',err.message))
+.then(()=>mongoDebug('MongoDB is UP.'))
+.catch(err=>mongoDebug('MongoDB is Down, raison :',err.message))
--- rest_mongo_db/index.js
+++ rest_mongo_db/index.js
@@ -1,1 +1,13 @@
 require('./db/connect')
+const express = require('express');
+const appDebug = require('debug')('app:debug')
+const student_router= require('./routers/students')
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('/api/students',student_router)
+
+
+app.listen(port, ()=> appDebug(`Server run on ${port}`))
