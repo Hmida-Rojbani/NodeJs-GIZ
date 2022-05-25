5eb32131865ccc8fc024a3d032e690453b961d13
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-05-25 16:03:00 +0100

    Base functions

--- rest_mongo_db/db/connect.js
+++ rest_mongo_db/db/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const mongoDebug = require('debug')('app:mongo');
 
-mongoose.connect('mongodb+srv://user:1234@cluster0.l9vwd.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://user:1234@cluster0.l9vwd.mongodb.net/school?retryWrites=true&w=majority')
 .then(()=>mongoDebug('MongoDB is UP.'))
 .catch(err=>mongoDebug('MongoDB is Down, raison :',err.message))
--- rest_mongo_db/routers/students.js
+++ rest_mongo_db/routers/students.js
@@ -1,4 +1,31 @@
 const router = require('express').Router();
+const {Student} = require('../models/student');
 
+//save student
+router.post('/', async (req,res)=>{
+
+    let student = new Student(req.body);
+    try {
+        student = await student.save();
+        res.send(student)
+    } catch (error) {
+        res.send(405).send(error.message);
+    }
+});
+
+//retreive all students
+router.get('/', async (req,res)=>{
+    let students = await Student.find();
+    res.send(students)
+
+})
+//find by Id
+router.get('/:id', async (req,res)=>{
+    let student = await Student.findById(req.params.id);
+    if(!student)
+        return res.status(404).send('Student not found');
+    res.send(student)
+
+})
 
 module.exports=router
