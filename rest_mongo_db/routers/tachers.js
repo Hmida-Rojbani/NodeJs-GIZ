35f9bc6fcb83a015a5783f9932250bbf555f20d6
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-05-27 12:29:46 +0100

    add try catch

--- rest_mongo_db/routers/tachers.js
+++ rest_mongo_db/routers/tachers.js
@@ -65,14 +65,25 @@ router.delete('/id/:id_teacher', async (req,res)=>{
     let teacher = await Teacher.findById(req.params.id_teacher);
     if(!teacher)
         return res.status(404).send('Teacher not found');
-
-        for (let index = 0; index < teacher.classrooms.length; index++) {
+        try {
+            for (let index = 0; index < teacher.classrooms.length; index++) {
             const cl = teacher.classrooms[index];
             let classRoom = await ClassRoom.findById(cl._id);
             classRoom.teachers = classRoom.teachers.filter(t => !t.equals(teacher._id));
             await classRoom.save();
         }
-        await Teacher.deleteOne({_id:req.params.id_teacher});
+        } catch (error) {
+            teacherDebugger('Error in savinge class after delete',error.message);
+            return res.status(500).send('Error in savinge class after delete')
+        }
+        try {
+            await Teacher.deleteOne({_id:req.params.id_teacher});
+        } catch (error) {
+            teacherDebugger('Error in deleting Teacher',error.message);
+            return res.status(500).send('Error in deleting Teacher');
+        
+        }
+        
         res.send(teacher);
         
 
